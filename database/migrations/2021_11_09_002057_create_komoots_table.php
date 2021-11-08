6afea460065afe24a79d3db08b04d5953681fdf8
Dzianis Kotau <me@dzianiskotau.com>
2021-11-09 00:57:44 +0300

    Initial migrations

---
+++ database/migrations/2021_11_09_002057_create_komoots_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('komoots', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->unique()->onUpdate('cascade')->onDelete('cascade');
+            $table->string('profile_url');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('komoots');
+    }
+};
---
+++ database/migrations/2021_11_09_002319_create_event_logs_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('event_logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('event_id');
+            $table->foreignId('organization_id');
+            $table->string('track_url');
+            $table->string('track_distance');
+            $table->dateTime('track_start_date');
+            $table->unsignedMediumInteger('track_duration');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('event_logs');
+    }
+};
