2243e3867886568bf6f15724ccd89da51b996fec
Dzianis Kotau <me@dzianiskotau.com>
2021-11-09 01:19:33 +0300

    Pivot migration

---
+++ database/migrations/2021_11_09_010832_create_user_event_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('user_event', function (Blueprint $table) {
+            $table->foreignId('user_id')->index()->onDelete('cascade');
+            $table->foreignId('event_id')->index()->onDelete('cascade');
+            $table->primary(['user_id', 'event_id']);
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('user_event');
+    }
+};
